b09a0596d10c0dcac0289af2587604b60eb44ed8
LUANDESSON DA CRUZ ALVES <luandesson.alves@aluno.senac.rio>
2025-10-08 11:45:03 -0300

    Salvar alterações antes do pull

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from cliente import views
-
+from cliente.views import cadastrar_cli, login_view, logout_view, menu_cli, buscar_endereco
 urlpatterns = [
-                  path('cadastrar_cli/', views.cadastrar_cli, name="cadastrar_cli"),
-                  path('login/', views.login_view, name='login'),
-                  path('sair/', views.logout_view, name='logout'),
-                  path('menu_cli/', views.menu_cli, name='menu_cli'),
-                  path('buscacepapi/', views.buscacepapi, name='buscacepapi'),
+                  path('cadastrar_cli/', cadastrar_cli, name="cadastrar_cli"),
+                  path('login/', login_view, name='login'),
+                  path('sair/', logout_view, name='logout'),
+                  path('menu_cli/', menu_cli, name='menu_cli'),
+                  path('buscar_endereco/', buscar_endereco, name='buscar_endereco'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
